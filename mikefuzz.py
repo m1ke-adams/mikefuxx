163022c5d190e9dc4f5638f1a18a7c1a38d2fded
Discord:  m1ke#0003 <87667679+m1ke-adams@users.noreply.github.com>
2021-08-20 14:06:54 +0300

    Create mikefuzz.py

---
+++ mikefuzz.py
@@ -0,0 +1,15 @@
+import requests
+urll=input("fuzzing yapılacak site url giriniz: ")
+wordlist=input("wordlist dosyasını giriniz (önerilen dirb.txt): ")
+dosya=open(wordlist,"r")
+icerik=dosya.read()
+dosya.close()
+header={"Cookie": "security=low; PHPSESSID=a0b29e43b154e8cf261c3a93686bdd94"}
+for i in icerik.split("\n"):
+    print(i)
+    url=urll+str(i)
+    sonuc=requests.get(url=url)
+    if "200" in str(sonuc.status_code):
+        print("bulunan dizinler; ",i)
+    else:
+        print("bulunamayan dizinler; ",i)
